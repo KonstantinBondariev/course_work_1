690018bee99e89e47f8ea792bc510c029c8a98fb
Konstantin Bondariev <bondariev.konstantin@gmail.com>
2022-08-09 09:24:13 +0300

    nav-bar__btn, nav-bar-list open - finished style

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,17 @@ $(document).ready(function () {
     $('.header-navbar__item').toggleClass('open');
   });
 });
+
+// $(document).ready(function () {
+//   $('.header-navbar__btn').on('click', function () {
+//     var click = $(this).data('clicks');
+//     if (click % 2 == 1) {
+//       $(this).toggleClass('open');
+//       $('.header-navbar__list').toggleClass('open');
+//       $('.header-navbar__item').toggleClass('open');
+//     } else {
+//       $('.header-navbar__item').toggleClass('close');
+//     };
+//     $(this).data('clicks', click + 1);
+//   });
+// });
